2edbd90fff9f33e1f722d78a9c0377f9512710ec
Vivek79781 <vivekdbz248@gmail.com>
2023-03-03 21:51:11 +0530

    update Test route done

--- routes/dataEntry.js
+++ routes/dataEntry.js
@@ -2,21 +2,25 @@ const express = require('express');
 const router = express.Router();
 const { query } = require('../utils/db');
 
-router.get('/', (req, res) => {
-    res.render('dataEntry/dashboard');
+router.get('/', async(req, res) => {
+    const patients = await query('select * from Patient');
+    const tests = await query(`select * from Test where testStatus='Pending'`);
+    res.render('dataEntry/dashboard', { patients, tests });
 });
 
 router.get('/patient/:id/updateTest', async(req, res) => {
     const { id } = req.params;
-    const tests = await query(`select * from Test where patientID='${id} AND status='pending'`);
+    const tests = await query(`select * from Test where patientID=${id} AND testStatus='Pending'`);
+    console.log(tests);
     res.render('dataEntry/updateTest', { id, tests });
 });
 
-router.post('/patient/:id/updateTest/:testID', async(req, res) => {
+router.put('/patient/:id/updateTest/:testID', async(req, res) => {
     const { id, testID } = req.params;
     const { testResult } = req.body;
-    // Update test result and status
-    await query(`update Test set testResult='${testResult}', status='completed' where testID='${testID}'`);
+    // Update test result and testStatus
+    await query(`update Test set testResult='${testResult}', testStatus='completed' where testID=${testID}`);
+    req.flash('success', 'Test result updated successfully');
     res.redirect(`/dataEntry/patient/${id}/updateTest`);
 });
 
