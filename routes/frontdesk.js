13b2d2c0674f54ed02622ff30d583cd44541c102
Vivek79781 <vivekdbz248@gmail.com>
2023-03-03 15:09:09 +0530

    some changes in admin and implemented frontdesk treatment part is done

--- routes/frontdesk.js
+++ routes/frontdesk.js
@@ -10,7 +10,9 @@ router.get('/', async(req, res) => {
     if(rooms.length === 0) {
         available = false
     }
-    res.render('frontdesk/dashboard',{patients,stays,available});
+    // Treatment Schedule
+    const treatments = await query(`select * from Treatment where treatmentStatus = 'Pending'`)
+    res.render('frontdesk/dashboard',{patients,stays,available,treatments});
 });
 
 router.get('/register', (req, res) => {
@@ -22,6 +24,61 @@ router.post('/register', async(req, res) => {
     const { Name, Address, Phone, email, DateOfBirth, Gender } = req.body;
     await query(`INSERT INTO Patient (Name, DOB, Gender, Address, Phone, email, Registration_Date) VALUES ('${Name}', '${DateOfBirth}', '${Gender}', '${Address}', '${Phone}', '${email}', '${new Date().toISOString().slice(0, 19).replace('T', ' ')}' )`)
     res.redirect('/frontdesk/register');
+});
+
+router.get('/patient/:id/discharge', async(req, res) => {
+    const { id } = req.params;
+    const stays = await query(`select * from Stay where PatientID = ${id} and DischargeDate is null`)
+    const stay = stays[0]
+    const room = await query(`select * from Room where RoomNumber = ${stay.roomNumber}`)
+    await query(`UPDATE Stay SET DischargeDate = '${new Date().toISOString().slice(0, 19).replace('T', ' ')}' WHERE PatientID = ${id} and DischargeDate is null`)
+    await query(`UPDATE Room SET status = 'Available' WHERE RoomNumber = ${stay.roomNumber}`)
+    res.redirect('/frontdesk');
+});
+
+router.get('/patient/:id/admit', async(req, res) => {
+    const { id } = req.params;
+    console.log(id);
+    const rooms = await query(`select * from Room where status = 'Available'`)
+    console.log(rooms);
+    await query(`INSERT INTO Stay (patientID, roomNumber, AdmitDate) VALUES (${id}, ${rooms[0].roomNumber}, '${new Date().toISOString().slice(0, 19).replace('T', ' ')}')`)
+    await query(`UPDATE Room SET status = 'Occupied' WHERE roomNumber = ${rooms[0].roomNumber}`)
+    res.redirect('/frontdesk');
+});
+
+router.get('/patient/:id/treatment', async(req, res) => {
+    const { id } = req.params;
+    const doctors = await query(`select * from Doctor where doctorID in (select userID from User where role = 'Doctor')`)
+    res.render('frontdesk/treatment',{id,doctors});
+});
+
+router.post('/patient/:id/treatment', async(req, res) => {
+    const { id } = req.params;
+    console.log(req.body);
+    const { doctorID, Description, TreatmentDate, TreatmentTime } = req.body;
+    DateTreatment = TreatmentDate + ' ' + TreatmentTime
+    const treatments = await query(`select * from Treatment where doctorID = ${doctorID} and treatmentDate = '${DateTreatment}'`)
+    if(treatments.length === 0) {
+        await query(`INSERT INTO Treatment (patientID, doctorID, Description, TreatmentDate, TreatmentStatus) VALUES (${id}, ${doctorID}, '${Description}', '${DateTreatment}', 'Pending')`)
+        req.flash('success', 'Treatment has been scheduled');
+        res.redirect('/frontdesk');
+    } else {
+        req.flash('error', 'Doctor is not available at this time');
+        res.redirect('/frontdesk/patient/:id/treatment');
+    }
+
+});
+
+
+router.get('/patient/:id/test', async(req, res) => {
+    const { id } = req.params;
+    res.render('frontdesk/test',{id});
+});
+
+
+router.get('/patient/:id/appointment', async(req, res) => {
+    const { id } = req.params;
+    res.render('frontdesk/appointment',{id});
 });
 
 module.exports = router;
