ffe37b530f8069913d4077e51576f2e43a30e95a
Nikhil Saraswat <nikhilsaraswat@kgpian.iitkgp.ac.in>
2023-03-05 13:39:30 +0530

    Added appointment list in Doctor Dashboarard

--- routes/doctor.js
+++ routes/doctor.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { query } = require('../utils/db');
 
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
     // req.user.userID = req.user.userID.toString()
     // console.log(req.user.Name);
     const id = req.user.userID;
@@ -15,22 +15,45 @@ router.get('/', async(req, res) => {
     console.log(doctor);
     const rooms = await query(`select * from Room where currentOccupancy < maxCapacity`)
     let available = true
-    if(rooms.length === 0) {
+    if (rooms.length === 0) {
         available = false
     }
+    // current date and time
+    const now = new Date();
+    const mysqlDateTime = now.toISOString().slice(0, 19).replace('T', ' ');
+
+    console.log(mysqlDateTime);
+    // show all appointments under the doctor which are not more than 30 minutes old than the current time and appointment has a Date attribute which is DateTIme in mysql
+    let appointments = await query(`select * from Appointment where doctorID = ${id} and Date > '${mysqlDateTime}'`);
+    // sort the appointments by priority in descending order
+    appointments.sort((a, b) => {
+        return b.priority - a.priority;
+    });
+    // All patients 
+    const all_patients = await query(`select * from Patient`)
+    // patient id and name mapping
+    let patient_map = new Map();
+    for (let i = 0; i < all_patients.length; i++) {
+        patient_map.set(all_patients[i].patientID, all_patients[i].Name);
+    }
+    // add the patient name to the appointment
+    for (let i = 0; i < appointments.length; i++) {
+        appointments[i].patientName = patient_map.get(appointments[i].patientID);
+    }
     // Treatment Schedule
+    console.log(appointments);
     const treatments = await query(`select * from Treatment where treatmentStatus = 'Pending'`)
-    res.render('doctor/dashboard',{doctor, patients, stays, available, treatments});
+    res.render('doctor/dashboard', { doctor, patients, stays, available, treatments, appointments });
 });
 
 
-router.get('/patient/:id/treatment', async(req, res) => {
+router.get('/patient/:id/treatment', async (req, res) => {
     const { id } = req.params;
     console.log(id);
     const patient = await query(`select * from Patient where patientID = ${id}`)
     const treatments = await query(`select Doctor.Name as Dname, Treatment.* from Treatment, Doctor where patientID = ${id} and  Treatment.doctorID = Doctor.doctorID`)
     console.log(treatments);
-    if(treatments.length === 0) {
+    if (treatments.length === 0) {
         // loop over patients and print the name
         for (let i = 0; i < patient.length; i++) {
             let str = patient[i].Name + ' has not undergone any treatment';
@@ -44,19 +67,19 @@ router.get('/patient/:id/treatment', async(req, res) => {
         treatments.sort((a, b) => {
             return new Date(a.treatmentDate) - new Date(b.treatmentDate);
         });
-        res.render('doctor/treatment',{treatments, patient});
+        res.render('doctor/treatment', { treatments, patient });
     }
 
 });
 
 
-router.get('/patient/:id/test', async(req, res) => {
+router.get('/patient/:id/test', async (req, res) => {
     const { id } = req.params;
     console.log(id);
     const patient = await query(`select * from Patient where patientID = ${id}`)
     const tests = await query(`select Test.* from Test where patientID = ${id}`)
     console.log(tests);
-    if(tests.length === 0) {
+    if (tests.length === 0) {
         // loop over patients and print the name
         for (let i = 0; i < patient.length; i++) {
             let str = patient[i].Name + ' has not undergone any test';
@@ -70,7 +93,7 @@ router.get('/patient/:id/test', async(req, res) => {
         tests.sort((a, b) => {
             return new Date(a.testDate) - new Date(b.testDate);
         });
-        res.render('doctor/test',{tests, patient});
+        res.render('doctor/test', { tests, patient });
     }
 
 });
